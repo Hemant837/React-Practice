fecdb34a92b511a15be3c13618b89fcdf174d1cd
Hemant Verma <vermahemant837@gmail.com>
2023-08-23 12:24:48 +0530

    first commit of the day

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item!</h2>
+      <div>
+        <li>Food Rs 100</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 20</li>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
